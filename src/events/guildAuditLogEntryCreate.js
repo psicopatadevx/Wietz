39bbede378b27bf326c2694ee5d78c6bff9e69a3
psicopatadevx <rahmansadibyrywen@gmail.com>
2026-09-07 18:56:08 +0600

    guildAuditLogEntryCreate.js

--- src/events/guildAuditLogEntryCreate.js
+++ src/events/guildAuditLogEntryCreate.js
@@ -1,1 +1,18 @@
 
+import { AuditLogEvent } from 'discord.js';
+import { handleAuditLogEntry } from '../services/antiNukeService.js';
+
+export default {
+    name: 'guildAuditLogEntryCreate',
+
+    async execute(entry, guild, client) {
+        if (!entry || !guild) return;
+
+        await handleAuditLogEntry({
+            entry,
+            guild,
+            client,
+            AuditLogEvent,
+        });
+    },
+};
